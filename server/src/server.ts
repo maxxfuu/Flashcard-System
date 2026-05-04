38ab5a8f11969676aae36f2c0ca61dbe488ba56d
maxxfuu <maxfu826@gmail.com>
2026-05-03 21:27:58 -0700

    fixing type system

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import express, { CorsOptions } from "express";
+import express from "express";
 import cors from "cors";
 import { flashcardsRoutes } from "../routes/flashcards";
 import { userRoutes } from "../routes/user";
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 3001;
 
 const allowedOrigins = ["http://localhost:3000"];
 
-const corsOptions: CorsOptions = {
-  origin(origin, callback) {
+const corsOptions: cors.CorsOptions = {
+  origin(origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) {
     if (!origin) return callback(null, true);
     if (allowedOrigins.includes(origin)) return callback(null, true);
     return callback(new Error(`Not allowed by CORS: ${origin}`));
